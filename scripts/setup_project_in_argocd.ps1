880422b9d43161e741a15612d5f02d800939720f
Carsten Scharfenberg <carsten.scharfenberg@zalf.de>
2023-09-21 15:32:11 +0200

    finally some progress again

---
+++ scripts/setup_project_in_argocd.ps1
@@ -0,0 +1,25 @@
+param (
+    [Parameter(Mandatory = $true)] [string] $cluster,
+    [Parameter(Mandatory = $true)] [string] $project,
+    [string] $environment= "${cluster}_${project}"
+)
+
+# figure out some paths
+$project_path = Split-Path $PSScriptRoot -Parent
+$environments_path = Join-Path $project_path "environments"
+$environment_path = Join-Path $environments_path -ChildPath $environment
+
+# Login to argocd
+$sops_env = "sops exec-env ${environment_path}\credentials\argocd_secrets.enc.yaml"
+Invoke-Expression "${sops_env} 'argocd login %ARGOCD_SERVER% --insecure --grpc-web --username=%ARGOCD_ADMIN_USER% --password=%ARGOCD_ADMIN_PASSWORD%'"
+
+# Install keycloak app
+argocd app create keycloak `
+    --repo git@github.com:fairagro/basic_infrastructure.git `
+    --revision HEAD `
+    --path "${project_path}\helmcharts\zalf-keycloak" `
+    --dest-server https://kubernetes.default.svc `
+    --project fairagro `
+    --dest-namespace fairagro-keycloak `
+    --values "${environment_path}\values\zalf-keycloak.yaml" `
+    --values "${environment_path}\values\zalf-keycloak.enc.yaml"
