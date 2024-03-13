042c4dd2697702c4b96b6c188b6b1c07d0ab010d
Carsten Scharfenberg <carsten.scharfenberg@zalf.de>
2024-03-13 13:34:28 +0000

    removed olf keycloack comment

--- scripts/setup_project_in_argocd.sh
+++ scripts/setup_project_in_argocd.sh
@@ -27,17 +27,6 @@ environment_path="$mydir/../environments/$environment"
 # Login to argocd
 sops exec-env "${environment_path}/credentials/argocd_secrets.enc.yaml" 'argocd login $ARGOCD_SERVER --insecure --grpc-web-root-path $ARGOCD_PREFIX --username=$ARGOCD_ADMIN_USER --password=$ARGOCD_ADMIN_PASSWORD'
 
-# Install keycloak app
-# argocd app create keycloak \
-#     --repo "git@github.com:fairagro/basic_infrastructure.git" \
-#     --revision HEAD \
-#     --path "helmcharts/zalf-keycloak" \
-#     --dest-server "https://kubernetes.default.svc" \
-#     --project fairagro \
-#     --dest-namespace fairagro-keycloak \
-#     --values "../../environments/${environment}/values/zalf-keycloak.yaml" \
-#     --values "../../environments/${environment}/values/zalf-keycloak.enc.yaml"
-
 # Install nextcloud app
 argocd app create nextcloud \
     --repo "git@github.com:fairagro/basic_infrastructure.git" \
