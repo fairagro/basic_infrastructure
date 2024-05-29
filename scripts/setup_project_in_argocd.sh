c50a01d63e65cfec6a253e3e8b8f823d1cd413b3
Carsten Scharfenberg <carsten.scharfenberg@zalf.de>
2024-05-29 13:40:38 +0000

    correct argocd setup when merged to main

--- scripts/setup_project_in_argocd.sh
+++ scripts/setup_project_in_argocd.sh
@@ -28,21 +28,21 @@ environment_path="$mydir/../environments/$environment"
 sops exec-env "${environment_path}/credentials/argocd_secrets.enc.yaml" 'argocd login $ARGOCD_SERVER --insecure --grpc-web-root-path $ARGOCD_PREFIX --username=$ARGOCD_ADMIN_USER --password=$ARGOCD_ADMIN_PASSWORD'
 
 # Install nextcloud app
-# argocd app create nextcloud \
-#     --repo "git@github.com:fairagro/basic_infrastructure.git" \
-#     --revision HEAD \
-#     --path "helmcharts/zalf-nextcloud" \
-#     --dest-server "https://kubernetes.default.svc" \
-#     --project fairagro \
-#     --dest-namespace fairagro-nextcloud \
-#     --values "../../environments/${environment}/values/zalf-nextcloud.yaml" \
-#     --values "../../environments/${environment}/values/zalf-nextcloud.enc.yaml" \
-#     --sync-option CreateNamespace=true
+argocd app create nextcloud \
+    --repo "git@github.com:fairagro/basic_infrastructure.git" \
+    --revision HEAD \
+    --path "helmcharts/zalf-nextcloud" \
+    --dest-server "https://kubernetes.default.svc" \
+    --project fairagro \
+    --dest-namespace fairagro-nextcloud \
+    --values "../../environments/${environment}/values/zalf-nextcloud.yaml" \
+    --values "../../environments/${environment}/values/zalf-nextcloud.enc.yaml" \
+    --sync-option CreateNamespace=true
 
 # Install zammad app
 argocd app create zammad \
     --repo "git@github.com:fairagro/basic_infrastructure.git" \
-    --revision zammad \
+    --revision HEAD \
     --path "helmcharts/zalf-zammad" \
     --dest-server "https://kubernetes.default.svc" \
     --project fairagro \
