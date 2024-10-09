d6486a14079467a6c3ecad641f08a5fc8674fd67
Carsten Scharfenberg <carsten.scharfenberg@zalf.de>
2024-10-09 13:41:56 +0000

    it partly work from local command line

--- docker/nextcloud-backup/src/main.py
+++ docker/nextcloud-backup/src/main.py
@@ -3,14 +3,16 @@ A backup script for Nextcloud, running in docker.
 """
 
 import logging
+#import subprocess
 from kubernetes import client, config
 from kubernetes.client import api
 from kubernetes.stream import stream
 
 # Usage example
-NAMESPACE = "fairagro-nexctloud"
+NAMESPACE = "fairagro-nextcloud"
 DEPLOYMENT_NAME = "fairagro-nextcloud"
 CONTAINER_NAME = "nextcloud"
+SERVICE_ACCOUNT = "nextcloud-backup-account"
 COMMAND = ["/var/www/html/occ", "maintenance:mode", "--on"]
 
 
@@ -29,6 +31,11 @@ def main():
     # * kubernetes.client.exceptions.ApiException
     # * kubernetes.config.config_exception.ConfigException
 
+    # kslogger = logging.getLogger('kubernetes')
+    # console_h = logging.StreamHandler()
+    # kslogger.addHandler(console_h)
+    # kslogger.setLevel(logging.DEBUG)
+
     # Set the logging level to DEBUG
     logging.basicConfig(level=logging.DEBUG)
 
@@ -40,10 +47,26 @@ def main():
         config.load_kube_config()
 
     # Create a Kubernetes API client configuration and enable debug output
-    configuration = client.Configuration()
-    configuration.debug = True
+    api_client = config.new_client_from_config()
+    api_client.configuration.debug = True
 
-    api_client = client.ApiClient(configuration=configuration)
+    # In case we're not running in a pod, the api_key is not set and has to be created
+    # by issuing token request to the authentication API.
+    # Therefore we assume that we have the required permissions.
+    if not api_client.configuration.api_key:
+        core_api = client.CoreV1Api(api_client)
+        body = client.AuthenticationV1TokenRequest(
+            spec=client.V1TokenRequestSpec(audiences=[""])
+        )
+        token = core_api.create_namespaced_service_account_token(SERVICE_ACCOUNT, NAMESPACE, body)
+        # command = ["kubectl", "create", "token", "-n", NAMESPACE, SERVICE_ACCOUNT]
+        # token = subprocess.check_output(command).decode("utf-8")
+        api_client.configuration.api_key['authorization'] = token.status.token
+        api_client.configuration.api_key_prefix['authorization'] = 'Bearer'
+        api_client.configuration.key_file = None
+        api_client.configuration.cert_file = None
+        api_client.configuration.username = "system:serviceaccount:" + NAMESPACE + ":" + SERVICE_ACCOUNT
+
     core_api = client.CoreV1Api(api_client)
     apps_api = api.AppsV1Api(api_client)
 
@@ -52,21 +75,22 @@ def main():
         DEPLOYMENT_NAME, NAMESPACE)
 
     # Get any pod associated with the deployment
-    pods = core_api.list_namespaced_pod(NAMESPACE, label_selector=f"app={
-                                        deployment.metadata.labels['app']}")
-    pod = any(pods.items)
+    pods = core_api.list_namespaced_pod(NAMESPACE, label_selector=f"app.kubernetes.io/name={
+                                        deployment.metadata.labels['app.kubernetes.io/name']}")
+    pod = pods.items[0]
 
     # Execute the command in the container
-    exec_command = ["/bin/sh", "-c"] + COMMAND
     resp = stream(core_api.connect_get_namespaced_pod_exec,
                   pod.metadata.name,
                   NAMESPACE,
-                  command=exec_command,
+                  container=CONTAINER_NAME,
+                  command=COMMAND,
                   stderr=True,
                   stdin=False,
                   stdout=True,
-                  tty=False,
-                  _preload_content=False)
+                  tty=False)
+
+    print(resp.output)
 
     while resp.is_open():
         resp.update(timeout=1)
