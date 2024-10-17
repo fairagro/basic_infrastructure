45d4e18669419a11d80257ad3b53e7e8ef63abd4
Carsten Scharfenberg <carsten.scharfenberg@zalf.de>
2024-10-17 11:40:09 +0000

    renamed app

--- scripts/setup_project_in_argocd.sh
+++ scripts/setup_project_in_argocd.sh
@@ -9,18 +9,15 @@ environment_path="$mydir/../environments/$environment"
 # Login to argocd
 sops exec-env "${environment_path}/credentials/argocd_secrets.enc.yaml" 'argocd login $ARGOCD_SERVER --insecure --grpc-web-root-path $ARGOCD_PREFIX --username=$ARGOCD_ADMIN_USER --password=$ARGOCD_ADMIN_PASSWORD'
 
-if [[ -f "${environment_path}/values/zalf-nextcloud.yaml" ]]; then
-    # Install nextcloud app
-    echo "Installing nextcloud app on ${environment}..."
-    argocd app create fairagro-nextcloud \
-        --upsert \
-        --repo "git@github.com:fairagro/basic_infrastructure.git" \
-        --revision HEAD \
-        --path "helmcharts/fairagro-applications" \
-        --dest-server "https://kubernetes.default.svc" \
-        --project fairagro \
-        --dest-namespace fairagro-applications \
-        --values "../../environments/${environment}/values/fairagro-applications.yaml" \
-        --sync-option CreateNamespace=true \
-        --sync-option Prune=true
-fi
+echo "Installing FAIRagro applications app on ${environment}..."
+argocd app create application-wrapper \
+    --upsert \
+    --repo "git@github.com:fairagro/basic_infrastructure.git" \
+    --revision HEAD \
+    --path "helmcharts/fairagro-applications" \
+    --dest-server "https://kubernetes.default.svc" \
+    --project fairagro \
+    --dest-namespace fairagro-applications \
+    --values "../../environments/${environment}/values/fairagro-applications.yaml" \
+    --sync-option CreateNamespace=true \
+    --sync-option Prune=true
