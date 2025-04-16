366cee42e0b6465a4ccd6207807b38c162263e65
Carsten Scharfenberg <carsten.scharfenberg@zalf.de>
2025-04-16 11:24:24 +0000

    added `kga` for 'kubernetes get all'

--- scripts/set_context.sh
+++ scripts/set_context.sh
@@ -71,6 +71,7 @@ source <(cmctl completion bash)
 alias k=kubectl
 alias d=docker
 alias kda="kubectl delete all,pdb,configmap,secret,pvc,ingress,serviceaccount,endpoints --all"
+alias kga="kubectl get all,pdb,configmap,secret,pvc,ingress,serviceaccount,endpoints"
 
 # Set bash completion for aliases
 complete -o default -F __start_kubectl k
